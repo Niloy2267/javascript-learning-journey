d7f7cb44c591d929772b6c0bd6f0429fb374664e
Niloy Barua <158665843+Niloy2267@users.noreply.github.com>
2025-10-19 22:43:31 +0600

    Add files via upload
    Variables and Console Practice

---
+++ first.js
@@ -0,0 +1,2 @@
+console.log("This is the first.js file");
+console.log("Niloy Barua")
---
+++ profile.js
@@ -0,0 +1,6 @@
+const profile = {
+    username: "Niloy Barua",
+    isFollow: false,
+    followes:890,
+    followings: 150,
+};
